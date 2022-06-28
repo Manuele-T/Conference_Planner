40326532c0206eb01d63365453b837ae79f672de
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-06-28 09:16:03 +0100

    server side code for Lab 8

---
+++ routes/routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/controllers");
+
+router.get("/", controller.listMenu);
+router.get("/food", controller.listMenu);
+router.post('/addOrder', controller.addOrder);
+router.get("/new", controller.newList);
+
+router.use(function(req, res) {
+        res.status(404);
+        res.type('text/plain');
+        res.send('404 Not found.');
+    });
+
+router.use(function(err, req, res, next) {
+        res.status(500);
+        res.type('text/plain');
+        res.send('Internal Server Error.');
+    });
+
+module.exports = router;
