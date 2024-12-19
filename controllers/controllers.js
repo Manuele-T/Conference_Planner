784a7c03a3c3c5567557a1705a6615786fc18c4a
Manu <manuele.tacchetti@gmail.com>
2024-12-19 20:13:47 +0000

    new data

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -95,12 +95,45 @@ exports.rateTalkById = function (req, res) {
 };
 
 exports.handlePosts = function (req, res) {
-  let talkId = req.body.talkId;
-  let newRating = req.body.rating;
-  conf
-    .rateTalk(talkId, newRating)
-    .then(console.log("rating added"))
+  const { talkId, rating } = req.body;  // Ignore userId completely
+
+  // Validate rating data
+  if (!talkId || !rating || rating < 1 || rating > 5) {
+    res.status(400).send("Invalid rating data");
+    return;
+  }
+
+  // Find the talk by its ID
+  conf.getTalkById(talkId)
+    .then((talk) => {
+      if (!talk || talk.length === 0) {
+        res.status(404).send("Talk not found");
+        return;
+      }
+
+      // Add the rating without any userId
+      talk[0].ratings.push(rating);
+
+      // Update the talk's ratings
+      conf.conf.update(
+        { id: talkId },
+        { $set: { ratings: talk[0].ratings } },
+        {},
+        (err, numReplaced) => {
+          if (err) {
+            console.error("Failed to update ratings", err);
+            res.status(500).send("Failed to update ratings");
+          } else {
+            console.log(`Rating ${rating} added for talk ${talkId}`);
+            res.status(201).send("Rating added successfully");
+          }
+        }
+      );
+    })
     .catch((err) => {
-      console.log("promise rejected", err);
+      console.error("Failed to find talk", err);
+      res.status(500).send("Failed to find talk");
     });
 };
+
+
